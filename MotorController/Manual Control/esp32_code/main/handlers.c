4b5d1e60548057bfe10153169bc46499f4fa6dfa
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2024-12-28 19:44:56 -0500

    added manual control starting example

---
+++ MotorController/Manual Control/esp32_code/main/handlers.c
@@ -0,0 +1,15 @@
+#include "handlers.h"
+#include "globals.h"
+#include "esp_log.h"
+#include <stdio.h>
+
+
+void handle_MOV_GPS(){
+    printf("Handle MOV GPS\n");
+}
+
+// TODO: Delete
+void handle_MOV_PWM(){
+    printf("Handle MOV PWM\n");
+}
+
---
+++ MotorController/Manual Control/esp32_code/main/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define TASK_STACK_SIZE (2048)             // Buffer size for UART communication
+
+#endif 
