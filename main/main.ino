fc1458d5e1d7ccd0b0f6802e4cc116e6a7d18288
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2025-02-18 10:57:43 -0500

    Added working code with two test functions

--- main/main.ino
+++ main/main.ino
@@ -20,8 +20,8 @@ int minUs = 1000;
 int maxUs = 2000;
 int frequency = 50; // 50Hz
 
-int motorpin1 = 15;      // GPIO pin used to connect the servo control (digital out) 
-int motorpin2 = 4;      // GPIO pin used to connect the servo control (digital out)  
+int motorpin1 = 13;      // GPIO pin used to connect the servo control (digital out) 
+int motorpin2 = 2;      // GPIO pin used to connect the servo control (digital out)  
 
 void setup() {
   // Start communication with the Serial Monitor (USB Serial)
@@ -32,6 +32,26 @@ void setup() {
 
   motor1.attach(motorpin1, minUs, maxUs);   // attaches the servo on pin 18 to the servo object
   motor2.attach(motorpin2, minUs, maxUs);   // attaches the servo on pin 18 to the servo object
+
+  xTaskCreatePinnedToCore(
+    task1,   // Function to run
+    "tast1",   // Name of the task
+    1000,            // Stack size (bytes)
+    NULL,            // Task input parameter
+    1,               // Task priority (1 is low)
+    NULL,            // Task handle
+    0                // Core to run on (Core 1)
+  );
+
+  xTaskCreatePinnedToCore(
+    task2,   // Function to run
+    "tast2",   // Name of the task
+    1000,            // Stack size (bytes)
+    NULL,            // Task input parameter
+    1,               // Task priority (1 is low)
+    NULL,            // Task handle
+    0                // Core to run on (Core 1)
+  );
 
   delay(1000); // short delay
 
@@ -50,80 +70,96 @@ void loop() {
 
       if (incomingByte == '\n') {
 
-        // Split String
-        int index = inputString.indexOf(' ');  // Find the position of the comma
-
-        String inputKey = inputString.substring(0, index);      // "Hello"
-        String OtherParts = inputString.substring(index + 1);    // "World"
-
-        // Startup Mode: STARTUP
-        if(inputKey == "STARTUP"){
-          STATE = "STARTUP";
-          Serial.println("In Startup Mode");
-
-            // Set both motors to stopped
-            motor1.write(90);                 
-            motor2.write(90);                  
-        }
-
-        // Manual Mode: MOV
-        else if(inputKey == "MOV"){
-
-          String direction = inputString.substring(index + 1); 
-          STATE = "MOV";
-
-          Serial.println("// MOV //");
-
-          Serial.print("The state is: ");
-          Serial.println(STATE);
-
-          Serial.print("The inputKey is: ");
-          Serial.println(inputKey);
-
-          Serial.print("The direction is: ");
-          Serial.println(direction);
-
-
-          // Testing up and down
-          if(direction == "w"){
-            Serial.println("Forward");
-            motor1.write(110);                 
-            motor2.write(110);  
-          }
-          else if(direction == "s"){
-            Serial.println("Backward");
-            motor1.write(70);                 
-            motor2.write(70);  
-          }
-          else {
-            Serial.println("Not sure what the direction is??");
-          }
-          
-          Serial.println("// MOV //");
-        }
-
-        // Test Mode: TEST
-        else if(inputKey == "TEST"){
-          Serial.println("In Test Mode");
-        }
-
-        // Base Case
-        else {
-          Serial.println("Invalid Input");
-        }
-
-        // GPS Mode: MOVGPS
-
-        // PWM Mode: MOVPWM
+        parseInput(inputString);
       
-      // Clear the string for the next line
-      inputString = "";
+        inputString = "";// Clear the string for the next line
       } else {
         // Otherwise, add the byte to the string
         inputString += incomingByte;
       }
       delay(100);
     }
+}
+
+
+
+// Parse the user input and move the motors
+void parseInput(String inputString){
+
+  int index = inputString.indexOf(' ');  // Find the position of the comma
+
+  String inputKey = inputString.substring(0, index);      // "Hello"
+  String OtherParts = inputString.substring(index + 1);    // "World"
+
+  // Startup Mode: STARTUP
+  if(inputKey == "STARTUP"){
+    STATE = "STARTUP";
+    Serial.println("In Startup Mode");
+
+      // Set both motors to stopped
+      motor1.write(90);                 
+      motor2.write(90);                  
+  }
+
+  // Manual Mode: MOV
+  else if(inputKey == "MOV"){
+
+    String direction = inputString.substring(index + 1); 
+    STATE = "MOV";
+
+    Serial.println("// MOV //");
+
+    Serial.print("The state is: ");
+    Serial.println(STATE);
+
+    Serial.print("The inputKey is: ");
+    Serial.println(inputKey);
+
+    Serial.print("The direction is: ");
+    Serial.println(direction);
+
+
+    // Testing up and down
+    if(direction == "w"){
+      Serial.println("Forward");
+      motor1.write(110);                 
+      motor2.write(110);  
+    }
+    else if(direction == "s"){
+      Serial.println("Backward");
+      motor1.write(70);                 
+      motor2.write(70);  
+    }
+    else {
+      Serial.println("Not sure what the direction is??");
+    }
+    
+    Serial.println("// MOV //");
+  }
+
+  // Test Mode: TEST
+  else if(inputKey == "TEST"){
+    Serial.println("In Test Mode");
+  }
+
+  // Base Case
+  else {
+    Serial.println("Invalid Input");
+  }
+}
+
+void task1(void *parameter) {
+  while (true) {
+    Serial.println("Reading Data 1");
+    vTaskDelay(1000);    
+  }
+}
+
+void task2(void *parameter) {
+  while (true) {
+    Serial.println("Reading Data 2");
+    vTaskDelay(1000);    
+  }
 }
 
 
