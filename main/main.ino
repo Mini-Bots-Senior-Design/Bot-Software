c6de068605564b72d0e8a5bd82d8bbfa36821c62
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2025-02-18 09:55:09 -0500

    added servo library

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,11 @@
 // File to control all onboard software
+#include <ESP32Servo.h> 
 
-// Move parsing funciton out of main file
-// Add motor movement code
 
-// Modes
+
+///////////
+// Modes //
+///////////
 // STARTUP
 // MOV
 // TEST
@@ -11,43 +13,29 @@
 // MOVPWM
 String STATE = "STARTUP";
 
-// Define PWM output pin
-#define ESC1_PWM_PIN 13  // Pin for Motor 1
-#define ESC2_PWM_PIN 2 // Pin for Motor 2
+Servo motor1;  // create servo object to control a servo
+Servo motor2; 
 
-// Define pulse width range (in microseconds)
-#define ESC_MIN_PULSEWIDTH_US 1000  // 1ms = Minimum speed (or stop)
-#define ESC_MAX_PULSEWIDTH_US 2000  // 2ms = Maximum speed
+int minUs = 1000;
+int maxUs = 2000;
+int frequency = 50; // 50Hz
 
-// Define PWM frequency parameters
-#define PWM_FREQUENCY_HZ 50   // 50Hz frequency (standard for ESCs/servos)
-#define PWM_PERIOD_MS   20    // 20ms period (1/Frequency)
-
-// Current pulse width (start at minimum)
-int pulseWidth = 1500; // Middle Range 
-
-int leftPulseWidth = 1500; // Middle Range 
-int rightPulseWidth = 1500; // Middle Range 
-
+int motorpin1 = 15;      // GPIO pin used to connect the servo control (digital out) 
+int motorpin2 = 4;      // GPIO pin used to connect the servo control (digital out)  
 
 void setup() {
   // Start communication with the Serial Monitor (USB Serial)
   Serial.begin(115200); // Standard baud rate for Serial Monitor
 
-  pinMode(ESC1_PWM_PIN, OUTPUT);
-  // pinMode(ESC2_PWM_PIN, OUTPUT);
+  motor1.setPeriodHertz(frequency);// Standard 50hz servo
+  motor2.setPeriodHertz(frequency);// Standard 50hz servo
 
-  // Create tasks and pin them to Core 1
-  xTaskCreatePinnedToCore(
-    motor1Control,   // Function to run
-    "Motor1 Task",   // Name of the task
-    1000,            // Stack size (bytes)
-    NULL,            // Task input parameter
-    1,               // Task priority (1 is low)
-    NULL,            // Task handle
-    1                // Core to run on (Core 1)
-  );
+  motor1.attach(motorpin1, minUs, maxUs);   // attaches the servo on pin 18 to the servo object
+  motor2.attach(motorpin2, minUs, maxUs);   // attaches the servo on pin 18 to the servo object
 
+  delay(1000); // short delay
+
+  Serial.println("Startup Completed");
 }
 
 // Task to be run on Core 1
@@ -73,10 +61,9 @@ void loop() {
           STATE = "STARTUP";
           Serial.println("In Startup Mode");
 
-          // TODO: add code to set pulseWidth to be 1500
-          pulseWidth = 1500;
-
-
+            // Set both motors to stopped
+            motor1.write(90);                 
+            motor2.write(90);                  
         }
 
         // Manual Mode: MOV
@@ -100,14 +87,16 @@ void loop() {
           // Testing up and down
           if(direction == "w"){
             Serial.println("Forward");
-            pulseWidth = 1700;
+            motor1.write(110);                 
+            motor2.write(110);  
           }
           else if(direction == "s"){
             Serial.println("Backward");
-            pulseWidth = 1300;
+            motor1.write(70);                 
+            motor2.write(70);  
           }
           else {
-            Serial.println("Not sure what the input is??");
+            Serial.println("Not sure what the direction is??");
           }
           
           Serial.println("// MOV //");
@@ -133,22 +122,10 @@ void loop() {
         // Otherwise, add the byte to the string
         inputString += incomingByte;
       }
-
       delay(100);
     }
 }
 
-
-// currently working
-void motor1Control(void *parameter) {
-  while (true) {
-    // Test
-    digitalWrite(ESC1_PWM_PIN, HIGH);  // Start PWM pulse
-    delayMicroseconds(pulseWidth);     // Wait for pulseWidth duration in milliseconds (non-blocking)
-    digitalWrite(ESC1_PWM_PIN, LOW);  // Start PWM pulse
-    delayMicroseconds(20000 - pulseWidth);     // Wait for pulseWidth duration in milliseconds (non-blocking)
-  }
-}
 
 
 
